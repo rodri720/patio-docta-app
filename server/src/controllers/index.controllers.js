043ef0640944490288f4a2da75fb0cad9cc92f71
rodri720 <tomiyrami@gamil.com>
2023-12-28 23:33:08 -0300

    rutas listas

--- server/src/controllers/index.controllers.js
+++ server/src/controllers/index.controllers.js
@@ -1,27 +1,72 @@
-const { Pool } = require ('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'patio-docta',
     password: 'admin',
-    port: '5432'
-})
-
+    port: 5432
+});
 
 const getUsers = async (req, res) => {
-   
-       const response = await pool.query('SELECT * FROM users');
-       res.status(200).json(response.rows);
+    try {
+        const response = await pool.query('SELECT * FROM users');
+        res.status(200).json(response.rows);
+    } catch (error) {
+        console.error('Error retrieving users:', error);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+};
+
+const getUsersById = async (req, res) => {
+    res.end =req.params.id;
+    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    res.json(200).json(response.rows);
+}
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        const query = 'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *';
+        const values = [name, email];
+        
+        const response = await pool.query(query, values);
+        console.log(response)
+        res.status(200).json({
+            message: 'User created successfully',
+            body: {
+                user: response.rows[0]
+            }
+        });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+};
+
+const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { name, email } = req.body;
+    const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id]);
+    console.log(response)
+    res.json('User Updated Successfully');
     
- }
- const createUser = async (req, res) => {
-     
- }
- 
+}
+const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    pool.query('DELETE FROM users WHERE id = $1', [id])
+    console.log(response)
+    res.json(`User ${id} deleted successfully`);
+}
 
 module.exports = {
     getUsers,
-    createUser
-    
-}
+    createUser,
+    getUsersById,
+    deleteUser,
+    updateUser
+};
--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,9 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
-const { getUsers, createUser } = require('../controllers/index.controllers');
+const { getUsers, createUser, getUsersById, deleteUser, updateUser } = require('../controllers/index.controllers');
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
+router.get('/users:id', getUsersById);
+router.post('/users', createUser);
+router.delete('/users:id', deleteUser);
+router.put('/users:id', updateUser);
 
 module.exports = router
