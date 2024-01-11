b7f7ca32b0cdcee539e51236e505ec9c53decccf
rodri720 <tomiyrami@gamil.com>
2024-01-10 22:31:42 -0300

    logout arreglado

--- server/src/routes/RoutesLogout.js
+++ server/src/routes/RoutesLogout.js
@@ -4,16 +4,13 @@ const { requiresAuth } = require('express-openid-connect');
 const router = express.Router();
 
 router.get('/', requiresAuth(), (req, res) => {
-  req.logout(); 
-
-  
+  req.logout();
   res.redirect('/');
 });
+
 router.post('/', requiresAuth(), (req, res) => {
-    req.logOut();
-  
-    
-    res.redirect("/");
-  });
-  
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
