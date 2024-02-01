b6c426a6b6c5f3f907d8b74b06c62de531796cf0
rodri720 <tomiyrami@gamil.com>
2024-02-01 02:13:00 -0300

    cambios en vite

--- my-vite-project/vite.config.js
+++ my-vite-project/vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ReactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ReactRefresh()],
   server: {
     proxy: {
       '/api': 'http://your-backend-server.com',
