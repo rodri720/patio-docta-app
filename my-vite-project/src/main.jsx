fcb436b0c4d14c67df86e84a81549d75303660bb
rodri720 <tomiyrami@gamil.com>
2024-02-01 22:17:22 -0300

    mas cambios

--- my-vite-project/src/main.jsx
+++ my-vite-project/src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root'));
+const root = document.getElementById("root");
 
-root.render(
+ReactDOM.createRoot(root).render(
   <ChakraProvider>
     <Auth0Provider
       domain="dev-pkerk343c7ujo0mk.us.auth0.com"
